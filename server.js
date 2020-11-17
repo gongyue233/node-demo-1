ff3f18eb261bb37fce3e5b6bb16a045c72e175da
baiyang <gongyue233@163.com>
2020-11-17 18:13:05 +0800

    3

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ var server = http.createServer(function(request, response){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`
-    <link rel="stylesheet" href="./x">
+    <link rel="stylesheet" href="./style.css">
     <h1>你好</h1>`)
     response.end()
-  } else if(path === '/x'){
+  } else if(path === '/style.css'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`h1{color: red}`)
